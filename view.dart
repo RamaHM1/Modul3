7a09a2bfbaafcfdd2f220acbbad777d8e8f2d373
Rama HAryamadha <55016964+RamaHM1@users.noreply.github.com>
2021-04-11 21:06:06 +0700

    Add files via upload

---
+++ view.dart
@@ -0,0 +1,110 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:modul3_app/messaging.dart';
+import 'package:modul3_app/api.dart';
+
+class MessagingWidget extends StatefulWidget {
+  @override
+  _MessagingWidgetState createState() => _MessagingWidgetState();
+}
+
+class _MessagingWidgetState extends State<MessagingWidget> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  final TextEditingController titleController =
+      TextEditingController(text: 'VirgiBCT');
+  final TextEditingController bodyController =
+      TextEditingController(text: 'Virgiesw');
+  final List<Message> messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.onTokenRefresh.listen(sendTokenToServer);
+    _firebaseMessaging.getToken();
+    _firebaseMessaging.subscribeToTopic('all');
+
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        final notification = message['notification'];
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: ListTile(
+              title: Text("${notification['title']}"),
+              subtitle: Text("${notification['body']}"),
+            ),
+          ),
+        );
+        setState(() {
+          messages.add(Message(
+              title: notification['title'], body: notification['body']));
+        });
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+
+        final notification = message['data'];
+        setState(() {
+          messages.add(Message(
+            title: '${notification['title']}',
+            body: '${notification['body']}',
+          ));
+        });
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        final notification = message['data'];
+        setState(() {
+          messages.add(Message(
+            title: '${notification['title']}',
+            body: '${notification['body']}',
+          ));
+        });
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+  }
+
+  @override
+  Widget build(BuildContext context) => ListView(
+        children: [
+          TextFormField(
+            controller: titleController,
+            decoration: InputDecoration(labelText: 'Title'),
+          ),
+          TextFormField(
+            controller: bodyController,
+            decoration: InputDecoration(labelText: 'Body'),
+          ),
+          RaisedButton(
+            onPressed: sendNotification,
+            child: Text('Send notification to all'),
+          ),
+        ]..addAll(messages.map(buildMessage).toList()),
+      );
+
+  Widget buildMessage(Message message) => ListTile(
+        title: Text(message.title),
+        subtitle: Text(message.body),
+      );
+
+  Future sendNotification() async {
+    final response = await Messaging.sendToAll(
+      title: titleController.text,
+      body: bodyController.text,
+    );
+
+    if (response.statusCode != 200) {
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content:
+            Text('[${response.statusCode}] Error message: ${response.body}'),
+      ));
+    }
+  }
+
+  void sendTokenToServer(String fcmToken) {
+    print('Token: $fcmToken');
+  }
+}
